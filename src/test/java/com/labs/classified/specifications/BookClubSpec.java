e378df6796dc4be82b6a11f4d117b6535e81451f
ogomb <mbogolewis@gmail.com>
2018-03-23 15:08:02 +0300

    chore(test-book): unit test book
    Assert that a book can be created and instantiated.

--- src/test/java/com/labs/classified/specifications/BookClubSpec.java
+++ src/test/java/com/labs/classified/specifications/BookClubSpec.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+
 
 public class BookClubSpec {
 
