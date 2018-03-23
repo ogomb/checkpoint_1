60d42e2f29d3eb724c913f36b524befa6f3066ae
ogomb <mbogolewis@gmail.com>
2018-03-23 13:05:20 +0300

    chore(test-member): create member object
    Test member object is created and instantiated.

---
+++ src/test/java/com/labs/classified/specifications/MemberSpec.java
@@ -0,0 +1,35 @@
+package com.labs.classified.specifications;
+
+import com.labs.classified.implementation.Member;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class MemberSpec {
+
+    private final String name ="Lewis Taylor";
+    private final int age = 30;
+    private final String address = "3131 NY";
+    private Member member;
+
+    @Before
+    public void beforeTest() {
+        member = new Member(name, age, address);
+    }
+
+    @Test
+    public void whenInstantiatedThenNameIsSet() {
+        assertEquals(member.getName(), name);
+    }
+
+    @Test
+    public void whenInstantiatedThenAgeIsSet() {
+        assertEquals(member.getAge(), age);
+    }
+
+    @Test
+    public void whenInstantiatedThenAddressIsSet(){
+        assertEquals(member.getAddress(), address);
+    }
+
+}
