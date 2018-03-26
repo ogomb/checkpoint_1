d20ce72b0171b6e7677015368e4f3ea7675d43b2
ogomb <mbogolewis@gmail.com>
2018-03-26 18:26:00 +0300

    feat(create-book): add book object
    Book object is created with its member variables and methods.

---
+++ src/main/java/com/labs/classified/implementation/Book.java
@@ -0,0 +1,31 @@
+package com.labs.classified.implementation;
+
+public class Book {
+    private String ISBNNumber;
+    private String title;
+    private String author;
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
