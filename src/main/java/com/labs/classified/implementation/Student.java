03ebf2b5974a24541556b72526acc00c4d1f3df4
ogomb <mbogolewis@gmail.com>
2018-03-27 09:55:24 +0300

    feat(student-object): create student object
    Student object is created with its member variables and methods.

--- src/main/java/com/labs/classified/implementation/Student.java
+++ src/main/java/com/labs/classified/implementation/Student.java
@@ -1,4 +1,46 @@
 package com.labs.classified.implementation;
 
-public class Student {
+public class Student implements Member {
+    private String name;
+    private int age;
+    private String address;
+    private String subjectInterest;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSubjectInterest() {
+        return subjectInterest;
+    }
+
+    public void setSubjectInterest(String subjectInterest) {
+        this.subjectInterest = subjectInterest;
+    }
 }
