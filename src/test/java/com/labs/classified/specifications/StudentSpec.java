7a707d24e4dc51e6a8f9e7f955cbd582464f5c17
ogomb <mbogolewis@gmail.com>
2018-03-27 10:03:00 +0300

    chore(refactor-specs): refactor member objects
    Student and Staff object refactored to implement member methods.

--- src/test/java/com/labs/classified/specifications/StudentSpec.java
+++ src/test/java/com/labs/classified/specifications/StudentSpec.java
@@ -7,6 +7,9 @@ import static org.junit.Assert.*;
 public class StudentSpec {
     private Student student;
     private final String subjectInterest = "Science";
+    private final String name = "Lewis Taylor";
+    private final String address = "1344 NY";
+    private final int age = 31;
 
     @Before
     public final void before(){
@@ -16,5 +19,20 @@ public class StudentSpec {
     @Test
     public void whenInstantiatedThenSubjectIntrestIsSet(){
         assertEquals(student.getSubjectIntrest(), subjectInterest);
+    }
+
+    @Test
+    public void whenInstantiatedThenNameIsSet() {
+        assertEquals(student.getName(), name);
+    }
+
+    @Test
+    public void whenInstantiatedThenAgeIsSet() {
+        assertEquals(student.getAge(), age);
+    }
+
+    @Test
+    public void whenInstantiatedThenAddressIsSet(){
+        assertEquals(student.getAddress(), address);
     }
 }
