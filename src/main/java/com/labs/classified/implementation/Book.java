c5c00877f11e3934e517c19284ccf11cb2462601
ogomb <mbogolewis@gmail.com>
2018-03-27 12:58:18 +0300

    instantiate a Book object upon creation.

--- src/main/java/com/labs/classified/implementation/Book.java
+++ src/main/java/com/labs/classified/implementation/Book.java
@@ -5,6 +5,11 @@ public class Book {
     private String title;
     private String author;
 
+    public Book(String title, String author, String ISBNNumber){
+        this.title = title;
+        this.author = author;
+        this.ISBNNumber= ISBNNumber;
+    }
     public String getISBNNumber() {
         return ISBNNumber;
     }
