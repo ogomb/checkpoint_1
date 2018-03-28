0c92995059ca634df7d91a2ab041fb9c6f80c7af
ogomb <mbogolewis@gmail.com>
2018-03-28 15:26:17 +0300

    implement issuance of a book.

--- src/main/java/com/labs/classified/implementation/BookClub.java
+++ src/main/java/com/labs/classified/implementation/BookClub.java
@@ -1,9 +1,6 @@
 package com.labs.classified.implementation;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class BookClub {
     public List<Student> studentMembers =new ArrayList<>();
@@ -31,13 +28,44 @@ public class BookClub {
         bookCollection.add(ISBNumber);
     }
 
-    public void borrowBook(Book book){
+    public String borrowBook(Book book, Member... members){
+        ArrayList<Staff> staffQueue =  new ArrayList<>();
+        ArrayList<Student> studentQueue = new ArrayList<>();
+
         String ISBNumber = book.getISBNNumber();
-        if (bookCollection.size() == 0){
-            throw new RuntimeException("No books currently in store");
-        } else {
-            removeBookFromCollection(book);
+        int peopleRequestingBook = members.length;
+        if (bookCollection.size() == 0) {
+            return "No books currently in store";
         }
+        if(peopleRequestingBook > 0) {
+                ArrayList<Member> queueOfBorrowers = new ArrayList<>(Arrays.asList(members));
+                for(Member member: queueOfBorrowers){
+                    if (member instanceof Student){
+                        studentQueue.add((Student) member);
+                    }else
+                    staffQueue.add((Staff) member);
+                }
+                Collections.sort(staffQueue);
+                Collections.sort(studentQueue);
+
+               for (String bookToBorrow: bookCollection){
+                   if (bookToBorrow.equals(ISBNumber)) {
+                       if (staffQueue.size() > 0){
+                           removeBookFromCollection(book);
+                           return "Book issued to " + staffQueue.get(0).getName();
+                       }else {
+                           removeBookFromCollection(book);
+                           return "Book issued to " + studentQueue.get(0).getName();
+                       }
+                   }else {
+                       return "Such book does not exist";
+                   }
+               }
+        }
+        if(peopleRequestingBook < 1){
+                return "You are not allowed to borrow a book";
+        }
+        return "Can't borrow this book." + staffQueue;
     }
 
     public void removeBookFromCollection(Book book){
